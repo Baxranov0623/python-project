c026f31dd54d4678d21772a4bd8228902b35dbc9
Bexruz <baxranovbexruz620@gmail.com>
2026-03-09 15:06:49 +0500

    transactions

--- main.py
+++ main.py
@@ -1,1 +1,149 @@
-print("Hello world")
+import uuid
+from datetime import datetime,timezone
+# ========= Definitions
+class Transaction:
+    def __init__(
+        self,
+        id: uuid.UUID,
+        type: str,
+        sender_name: str,
+        receiver_name: str,
+        amount: int,
+        status: str,
+        datetime: datetime,
+    ) -> None:
+        self.id = id
+        self.type = type
+        self.sender_name = sender_name
+        self.receiver_name = receiver_name
+        self.amount = amount
+        self.status = status
+        self.datetime = datetime
+
+    def __str__(self) -> str:
+        return f"Transition <id={str(self.id)}>"
+
+
+class BankAccount:
+    def __init__(
+        self,
+        id: uuid.UUID,
+        name: str,
+        account_number: str,
+        balance: int,
+        created_at: datetime,
+    ) -> None:
+        self.id = id
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
+        self.created_at = created_at
+        self.transaction: list[Transaction] = []
+
+    def __str__(self):
+        return f"BankAccount <name= {self.name}>"
+
+
+
+
+# =====Globals
+current_account=None
+accounts: list[BankAccount] = []
+menu_text: str = """
+===Bank System Menu===
+1.Craete New Account
+2.View All Accunts
+3.Search Account
+4.Deposit
+5.Withdraw
+6.View Transactions
+7.Delete Account
+8.Exit
+======================
+"""
+# =================  Core logic
+def create_account():
+    print("Siz yangi akkaunt ochmoqchisi. Quyidagi ma'lumotlarni kiriting: "  )
+    name:str=input("F.I.Sh: ")
+    account_number:str=input("Akkaunt raqamingiz: ")
+    balance:int=int(input("Mavjud balansingiz: "))
+    id:uuid.UUID=uuid.uuid4()
+    create_at:datetime=datetime.now(timezone.utc)
+    new_account:BankAccount=BankAccount(
+        id=id,
+        name=name,
+        account_number=account_number,
+        balance=balance,
+        created_at=create_at
+    )
+
+    global accounts
+    accounts.append(new_account)
+    return new_account
+
+def view_all_account():
+    print("Barcha akkauntlar ro'yxati ")
+    for i, account in enumerate(accounts):
+        print(f'{i+1}.{account.name} - {account.balance}')
+    is_end:str=input("Operatsiya yakunlanishini xohlaysizmi?")
+    if is_end=='\n':
+        return None
+
+
+def search_account():
+    search_name:str=input('Qidirilayotgan akkaunt egasini kiriting: ')
+    print("=====Natijalar=====")
+    for i, account in enumerate(accounts):
+        if search_name.lower() in account.name.lower():
+            print( f'{i+1}.{str(account.id)} - {account.name} - {account.account_number}')
+    is_end:str=input("Operatsiya yakunlanishini xohlaysizmi?")
+    if is_end=='\n':
+        return None
+
+
+
+def deposity():
+    pass
+
+def withdraw():
+    pass
+
+def view_transactions():
+    pass
+
+def delete_account():
+    pass
+
+
+
+# ================= Main
+def main_menu():
+  
+    while True:
+        print(menu_text)
+        choice:int=int(input('Enter your choice: '))
+        if choice==1:
+           global current_account
+           current_account=create_account()
+          
+           print('Akkunt yaratildi! ')
+        elif choice==2:
+            view_all_account()
+        elif choice==3:
+            search_account()
+        elif choice==4:
+            deposity()
+        elif choice==5:
+            withdraw()
+        elif choice==6:
+            view_transactions()
+        elif choice==7:
+            delete_account()
+        else:
+            print('Exit')
+            break
+
+
+
+if __name__ == "__main__":
+    main_menu()
